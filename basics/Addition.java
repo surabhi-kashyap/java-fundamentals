13cc444228cdddbfb7a8b22ed391e61a3e4b038b
Surabhi <surabhikashyap@gmail.com>
2026-02-28 20:42:37 +0530

    Fixed compilation error

--- basics/Addition.java
+++ basics/Addition.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Example{
+public class Addition{
     public static void main(String ar[]){
         Scanner sc=new Scanner(System.in);
         System.out.print("enter first number");
